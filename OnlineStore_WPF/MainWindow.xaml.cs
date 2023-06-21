0fe6ea5a3973ef26fe844c7e6495b276989f9306
Ibrahim <ibrahimasadov32@gmail.com>
2023-06-22 00:45:44 +0400

    Online Store

--- OnlineStore_WPF/MainWindow.xaml.cs
+++ OnlineStore_WPF/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Windows.Themes;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,18 +23,77 @@ namespace OnlineStore_WPF
     {
         public ObservableCollection<Product> products { get; set; } = new();
 
-
+        public List<ListBoxItem>? MyProperty { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
+            MyProperty = new();
+            
+           
+           
             DataContext = this;
 
         }
 
         private void basketButton_Click(object sender, RoutedEventArgs e)
         {
+            // Prompt for product details
+            string productName = Microsoft.VisualBasic.Interaction.InputBox("Enter the product name:", "Product Name", "");
+            string productPriceString = Microsoft.VisualBasic.Interaction.InputBox("Enter the product price:", "Product Price", "");
+            decimal productPrice;
+
+            // Validate and convert the price to decimal
+            if (!decimal.TryParse(productPriceString, out productPrice))
+            {
+                MessageBox.Show("Invalid product price. Please enter a valid decimal value.");
+                return;
+            }
+
+            string imagePath = Microsoft.VisualBasic.Interaction.InputBox("Enter the image path or URL:", "Image Path or URL", "");
+
+            ListBoxItem newItem = new ListBoxItem
+            {
+                Content = new Grid
+                {
+                    Children = {
+                new Border { Style = (Style)FindResource("imageBorder") },
+                new Image { Source = CreateImageSource(imagePath), Width = 40 },
+                new Grid
+                {
+                    Margin = new Thickness(0, 183, 0, 0),
+                    Children =
+                    {
+                        new Border { Style = (Style)FindResource("productsPriceBorder") },
+                        new TextBlock { Text = productName, TextAlignment = TextAlignment.Center, VerticalAlignment = VerticalAlignment.Center, FontSize = 18, Foreground = Brushes.White, FontFamily = new FontFamily("Garamond") }
+                    }
+                }
+            }
+                }
+            };
+            
+            itemListBox.Items.Add(newItem);
+            Product selectedProduct = new Product();
+            selectedProduct.Name = productName;
+            selectedProduct.Price = productPrice;
+            MessageBox.Show($"Seçilen ürün: {selectedProduct.Name}\nQiymet: {selectedProduct.Price} Manat");
         }
+
+        private ImageSource CreateImageSource(string path)
+        {
+            if (Uri.TryCreate(path, UriKind.Absolute, out Uri uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
+            {
+                return new BitmapImage(uriResult);
+            }
+            else
+            {
+                return new BitmapImage(new Uri(path, UriKind.Relative));
+            }
+        }
+
+
+
+
 
         private void exitButton_Click(object sender, RoutedEventArgs e)
         {
@@ -42,6 +103,7 @@ namespace OnlineStore_WPF
 
         private void itemListBox_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
+            ListBoxItem selectedItem = itemListBox.SelectedItem as ListBoxItem;
             if (e.RightButton == MouseButtonState.Pressed)
             {
                 if (itemListBox.SelectedItem == item1)
@@ -68,10 +130,17 @@ namespace OnlineStore_WPF
                 {
                     itemListBox.Items.Remove(item6);
                 }
+                else
+                {
+                    itemListBox.Items.Remove(selectedItem);
+                }
+              
             }
         }
 
-        private void itemListBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+       
+
+        private void itemListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ListBox listBox = (ListBox)sender;
             ListBoxItem selectedItem = listBox.SelectedItem as ListBoxItem;
@@ -127,16 +196,13 @@ namespace OnlineStore_WPF
                     selectedProduct.Price = 0.65m;
                     MessageBox.Show($"Seçilen urun: {selectedProduct.Name}\nQiymet: {selectedProduct.Price} Manat");
 
-            
 
                 }
-         
+             
 
-            
+
 
             }
-
-
         }
     }
 }
